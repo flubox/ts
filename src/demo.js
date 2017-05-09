d0a56a88851e0c4d1884fd56e08ab1924a0edd97
flubox <francois.lunette@photobox.com>
2017-05-09 16:51:10 +0200

    demo source

--- src/demo.js
+++ src/demo.js
@@ -1,15 +1,51 @@
 import ts from './index';
 
-document.addEventListener('DOMContentLoaded', () => {
-    document.addEventListener('ts.started', () => console.info('event: ts started'));
-    document.addEventListener('ts.onClick', ({target}) => console.info(`event onClick`, target));
-});
+const trad = {
+    "0": {
+        "en_UK": {
+            "title": "lorem"
+        }
+    },
+    "1": {
+        "en_UK": {
+            "title": "ipsum"
+        }
+    },
+    "2": {
+        "en_UK": {
+            "title": "dolor"
+        }
+    },
+    "3": {
+        "en_UK": {
+            "title": "sit"
+        }
+    },
+    "4": {
+        "en_UK": {
+            "title": "amet"
+        }
+    },
+    "5": {
+        "en_UK": {
+            "title": "consectetur"
+        }
+    }
+};
 
-const promises = ts({
-    debug: true,
+const debug = window.location.search && !!window.location.search.match(/debug/);
+
+const options = {
+    debug,
     domElement: '#root',
     locale: 'en_UK',
     resolve: id => console.info(`resolve id: ${id}`),
     reject: err => console.warn('err:', err),
-    translate: (id, locale) => require('../trad.json')[id][locale]
-});
+    translate: (id, locale) => ({...trad[id][locale], button: 'choose this theme'})
+};
+
+if (debug) {
+    console.info('debug', options);
+}
+
+const promises = ts(options);
