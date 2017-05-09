59b5c2a4fe75af1b19f3f0832106f02db962476a
flubox <francois.lunette@photobox.com>
2017-05-09 16:52:18 +0200

    Title component

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import {titlelizeAll} from '../helper';
 
-export const Title = ({title}) => title && <h3 className="ts-content-title">{title}</h3>;
+export const Title = ({title, onClick}) => title && <h3 className={`ts-content-title${onClick ? ' ts-clickable': ''}`}>{titlelizeAll(title)}</h3>;
 
 export default Title;
