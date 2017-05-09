396874ed417bf07f9b2005ef98ee1df4e60e0875
flubox <francois.lunette@photobox.com>
2017-05-09 16:53:07 +0200

    content builder

--- src/ContentBuilder.js
+++ src/ContentBuilder.js
@@ -2,9 +2,8 @@ import React from 'react';
 import {merge} from './helper';
 import Content from './containers/Content';
 
-export const ContentBuilder = props => (data, key) => {
-    console.info('...', 'ContentBuilder', {...props}, {...data});
-    return !!props && !!data && !!key && <Content key={key} {...merge(props)(data)}/>;
-};
+const t = a => typeof(a) !== 'undefined';
+
+export const ContentBuilder = props => (data, key) => t(props) && t(data) && t(key) && <Content key={key} {...merge(props)(data)}/>;
 
 export default ContentBuilder;
