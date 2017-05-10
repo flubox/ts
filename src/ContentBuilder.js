beaba2c587f2593c66ddc697559b5379cee2a34d
François Lunette (Photobox) <Francois.lunette@photobox.com>
2017-05-10 17:35:10 +0200

    Fix (#14)
    * overflow removed on demo page
    
    * bugfix on ContentBuilder failsafe check
    
    * ContentBuilder check method tested
    
    * dist

--- src/ContentBuilder.js
+++ src/ContentBuilder.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {merge} from './helper';
 import Content from './containers/Content';
 
-export const ContentBuilder = props => (data, key) => !!props && !!data && !!key && <Content key={key} {...merge(props)(data)}/>;
+export const check = item => typeof item !== 'undefined';
+
+export const ContentBuilder = props => (data, key) => check(props) && check(data) && check(key) && <Content key={key} {...merge(props)(data)}/>;
 
 export default ContentBuilder;
