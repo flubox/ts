6afb162035eb2c1081b0cec571543f7fc5de2558
flubox <francois.lunette@photobox.com>
2017-05-11 17:10:13 +0200

    Safer checking for description

--- src/components/Description.js
+++ src/components/Description.js
@@ -1,5 +1,6 @@
 import React from 'react';
+import {isDef, titlelize} from '../helper';
 
-export const Description = ({description}) => description && <div className="ts-content-description">{description}</div>;
+export const Description = ({description}) => isDef(description) && <div className="ts-content-description">{titlelize(description)}</div>;
 
 export default Description;
