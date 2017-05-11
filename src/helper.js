058237dde14b91971a3d49f2bc2390a26851d481
François Lunette (Photobox) <Francois.lunette@photobox.com>
2017-05-11 17:12:37 +0200

    Dev (#18)
    * wip webpack config
    
    * wip
    
    * first draft json schema content for theme selector
    
    * wip
    
    * external useless file removed
    
    * demo page
    
    * css file
    
    * demo source
    
    * Content Preview (for img) with props helper
    
    * Content container
    
    * Title component
    
    * GridSelector container
    
    * Button component
    
    * demo data file for api.js
    
    * content builder
    
    * tests
    
    * helpers
    
    * dist
    
    * Css align each (#3)
    
    * grid adjust for mobile and such
    
    * dist
    
    * Feat cleaning (#5)
    
    * script for post install: build
    
    * dist/* added to .gitignore
    
    * Feat cleaning (#6)
    
    * script for post install: build
    
    * dist/* added to .gitignore
    
    * removing dist files
    
    * Feat title on top (#8)
    
    * demo header margin adjust
    
    * button vertical margin
    
    * title on top
    
    * Tests (#10)
    
    * wip tests
    
    * wip
    
    * dist
    
    * minimalistic README file (#12)
    
    * Fix (#14)
    
    * overflow removed on demo page
    
    * bugfix on ContentBuilder failsafe check
    
    * ContentBuilder check method tested
    
    * dist
    
    * Update README.md
    
    * Tests (#17)
    
    * npm-debug.log added to .gitignore
    
    * sinon.js added to package(dev)
    
    * endpoint added to demo.js
    
    * contants.js for fetch related data
    
    * safer checking for button
    
    * various helpers added
    
    * externals f() for GridSelector fetch related operations
    
    * safer checking for Title
    
    * Save checking for ContentPreview (img)
    
    * Safer checking for description
    
    * update on tests
    
    * dist

--- src/helper.js
+++ src/helper.js
@@ -1,6 +1,6 @@
-export const domElementChecker = selector => new Promise((resolve, reject) => !!getNode(selector)(document) ? resolve(node) : reject(node));
+export const domElementChecker = document => domElement => new Promise((resolve, reject) => !!getNode(domElement)(document) ? resolve(domElement) : reject(domElement));
 
-export const factory = list => props => builder => !!builder && !!props && !!list && isArr(list) && list.filter(({preview}) => !!preview).map(builder(props));
+export const factory = list => props => builder => isDef(builder) && isDef(props) && isDef(list) && isArr(list) && list.length > 0 && list.filter(({preview}) => isDef(preview)).map(builder(props));
 
 export const getNode = selector => document => typeof selector === 'string' ? document.querySelector(selector) : selector;
 
@@ -8,7 +8,9 @@ export const id = a => i => ({...a, id: i});
 
 export const isArr = item => Array.isArray(item);
 
-export const isUrl = data => data && !!data.match(/^(http)(s?):/);
+export const isDef = item => !unDef(item);
+
+export const isUrl = data => isDef(data) && !!data.toString().match(/^(http)(s?):/);
 
 export const merge = a => b => !!a && !!b ? ({...a, ...b}) : a || b;
 
@@ -16,10 +18,12 @@ export const mergeByKeys = object => (accumulator, k) => ({...accumulator, [k]: 
 
 export const preview = a => p => ({...a, preview: p});
 
-export const props = ({className, clickable, id, onClick, preview, type}) => ({id, onClick, className: `${className}${onClick ? clickable : ''}`, data: preview, key: preview, src: preview, type });
+export const props = ({className, clickable, id, onClick, preview, type}) => ({id, onClick, className: `${className}${clickable && onClick ? clickable : ''}`, data: preview, key: `${id}${preview}`, src: preview, type });
 
 export const standardize = object => Object.keys(object).reduce((a, key) => isUrl(object[key]) ? preview(a)(object[key]) : id(a)(object[key]), {});
 
 export const titlelize = text => `${text.substr(0, 1).toUpperCase()}${text.substr(1)}`;
 
 export const titlelizeAll = text => text.split(' ').map(titlelize).join(' ');
+
+export const unDef = item => typeof item === 'undefined';
--- test/helper.js
+++ test/helper.js
@@ -1,19 +1,26 @@
 import {
+    domElementChecker,
     factory,
     getNode,
     getRenderProps,
     id,
     isArr,
+    isDef,
     isUrl,
     merge,
     mergeByKeys,
-    metaConfig,
     preview,
+    props,
     standardize,
     titlelize,
-    titlelizeAll
+    titlelizeAll,
+    unDef
 } from '../src/helper';
 import test from 'ava';
+import {shallow} from 'enzyme';
+
+// domElementChecker
+test('domElementChecker return a Promise', t => t.true(domElementChecker({querySelector: a => a})('lorem') instanceof Promise));
 
 // factory
 test('factory return false if neither list, props nor builder provided', t => t.false(factory()()()));
@@ -21,9 +28,10 @@ test('factory return false if neither list not builder provided', t => t.false(f
 test('factory return false if neither props nor builder provided', t => t.false(factory([{a: 'b'}])()()));
 test('factory return false if no builder provided', t => t.false(factory([{a: 'b'}])({c: 'd'})()));
 test('factory return false if no list provided', t => t.false(factory()({c: 'd'})(a => a)));
+test('factory return false if list is empty', t => t.false(factory([])({b: "b"})(a => a)));
 test('factory return false if no props provided', t => t.false(factory([{a: 'b'}])()(a => a)));
 // test('factory return false if the list provided is empty', t => {
-//     const result = factory([])({b: "b"})(a => a);
+//     const result = factory([{}])({b: "b"})(a => a);
 //     t.is(result.length, 0);
 // });
 
@@ -38,6 +46,10 @@ test('id({lorem: "ipsum"})(42) return {lorem: "ipsum", id: 42}', t => t.is(id({l
 test('isArr return true for: []', t => t.true(isArr([])));
 test('isArr return false for: "lorem" ', t => t.false(isArr("lorem")));
 test('isArr return false for: 42 ', t => t.false(isArr(42)));
+
+// isDef
+test('isDef() return false', t => t.false(isDef()));
+test('isDef("") return true', t => t.true(isDef('')));
 
 // isUrl
 test('isUrl return true for: http://lorem.ipsum', t => t.true(isUrl('http://lorem.ipsum')));
@@ -54,6 +66,13 @@ test('mergeByKeys', t => t.is(mergeByKeys({a: 'a'})({b: 'b'}).b, 'b'));
 // preview
 test('preview({lorem: "ipsum"})("dolor")', t => t.is(preview({lorem: 'ipsum'})('dolor').preview, 'dolor'));
 
+// props
+test(`props({className}).className === className`, t => t.is(props({className: 'lorem'}).className, 'lorem'));
+test(`props({preview}).data === preview`, t => t.is(props({preview: 'lorem'}).data, 'lorem'));
+test(`props({id, preview}).key === id + preview`, t => t.is(props({id:42, preview: 'lorem'}).key, '42lorem'));
+test(`props({preview}).src === preview`, t => t.is(props({preview: 'lorem'}).src, 'lorem'));
+test(`props({className, clickable, onClick}).className === className + clickable`, t => t.is(props({className: 'lorem', clickable: ' ipsum', onClick: 'a'}).className, 'lorem ipsum'));
+
 // standardize
 test('standardize({lorem: "42", ipsum: "http://lorem.ipsum"}) return {id: "42" preview: "http://lorem.ipsum"}', t => {
     const o = {lorem: '42', ipsum: 'http://lorem.ipsum'};
@@ -67,3 +86,7 @@ test('titlelize("lorem") return "Lorem"', t => t.is(titlelize('lorem'), 'Lorem')
 
 // titlelizeAll
 test('titlelizeAll("lorem ipsum") return "Lorem Ipsum"', t => t.is(titlelizeAll('lorem ipsum'), 'Lorem Ipsum'));
+
+// unDef
+test('unDef() return true', t => t.true(unDef()));
+test('unDef({}) return false', t => t.false(unDef({})));
