6cab4618a9da1f1795700a63fe509a4cef644f01
flubox <francois.lunette@photobox.com>
2017-05-11 17:09:42 +0200

    safer checking for Title

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import {titlelizeAll} from '../helper';
+import {isDef, titlelizeAll} from '../helper';
 
-export const Title = ({title, onClick}) => title && <h3 className={`ts-content-title${onClick ? ' ts-clickable': ''}`}>{titlelizeAll(title)}</h3>;
+export const Title = ({title}) => isDef(title) && <h3 className='ts-content-title'>{titlelizeAll(title)}</h3>;
 
 export default Title;
