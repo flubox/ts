abbe2c671f434d42923fb64ba80869f4f227347d
flubox <francois.lunette@photobox.com>
2017-05-11 17:10:27 +0200

    update on tests

--- test/Button.js
+++ test/Button.js
@@ -1,17 +1,26 @@
 import React from 'react';
 import test from 'ava';
 import {shallow, mount} from 'enzyme';
+import sinon from 'sinon';
 import Button from '../src/components/Button';
 
 const props = {
     button: 'ok',
     id: 42,
-    onClick: a => a
+    onClick: console.info
 };
 
 const className = 'ts-button';
 
 const clickable = 'ts-clickable';
+
+test('Button without props return null', t => t.is(shallow(<Button/>).type(), null));
+
+test('Button without id return null', t => t.is(shallow(<Button {...props} id={undefined}/>).type(), null));
+
+test('Button without "button" text return null', t => t.is(shallow(<Button {...props} button={undefined}/>).type(), null));
+
+test('Button without onClick return null', t => t.is(shallow(<Button {...props} onClick={undefined}/>).type(), null));
 
 test('Button is a div', t => t.is(shallow(<Button {...props}/>).type(), 'button'));
 
