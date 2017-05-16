8ac68c45110cd9a1005fb808745cee33f9cc4312
François Lunette (Photobox) <Francois.lunette@photobox.com>
2017-05-16 10:02:08 +0200

    demo with assets (#21)

--- src/api.js
+++ src/api.js
@@ -1,5 +1,8 @@
 var restify = require('restify');
 var db = require('./demo.json');
+var fs = require('fs');
+var img = fs.readdirSync(__dirname + '/../demo/');
+console.info('...', 'img', img);
  
 var server = restify.createServer({
   name: 'fake api',
@@ -9,6 +12,25 @@ server.use(restify.CORS());
 server.use(restify.acceptParser(server.acceptable));
 server.use(restify.queryParser());
 server.use(restify.bodyParser());
+server.get('/fakeimg/:filename', function (req, res) {
+  var filename = req.params.filename;
+  fs.exists(__dirname + '/../demo/' + filename, function (exists) {
+    if (exists) {
+      fs.readFile(__dirname + '/../demo/' + filename, function(err, data) {
+        if (err) {
+          res.send(500, err);
+          return;
+        } else {
+          res.setHeader('Content-Type', 'image/png');
+          res.send(data);
+        }
+      });
+    } else {
+      res.send(404, new Error('not found'));
+    }
+  });
+});
+
 server.get('/fakeapi', function (req, res) {
   if (db) {
     res.send(db);
