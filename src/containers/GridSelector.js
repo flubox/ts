3ba930d120cac86a67e8719aad761a206a7c2b7b
François Lunette (Photobox) <Francois.lunette@photobox.com>
2017-06-02 12:25:40 +0200

    Dev (#31)
    * /dist from .gitignore for demo purpose

--- src/containers/GridSelector.js
+++ src/containers/GridSelector.js
@@ -1,40 +1,51 @@
 import React, {PropTypes, Component} from 'react';
 import autobind from 'autobind-decorator';
-import {factory, isDef, standardize} from '../helper';
+import {factory, isDef, standardize, unDef} from '../helper';
 import ContentBuilder from '../ContentBuilder';
 import {fetchInit} from '../constants';
 import { Grid, Row } from 'react-flexbox-grid';
 
-export const standardizeState = context => data => context.setState({data: data.map(standardize)});
+export const sort = sortFunction => data => typeof sortFunction === 'function' ? data.sort(sortFunction) : data;
+
+export const standardizeState = context => data => context.setState({data: sort(context.props.options.sort)(data).map(standardize)});
 
 export const afterFetch = context => response => response.json().then(standardizeState(context));
 
-export const updateStateFromFetch = context => {
-    return isDef(context.props.options.endpoint) ? fetch(context.props.options.endpoint, fetchInit).then(afterFetch(context)).catch(console.warn) : false;
-};
+export const checkEndpoint = context => isDef(context.props.options.endpoint);
+
+export const updateStateFromFetch = context => checkEndpoint(context) && fetch(context.props.options.endpoint, fetchInit).then(afterFetch(context)).catch(console.warn);
 
 @autobind
 export class GridSelector extends Component {
     state = {data: []}
     constructor(props) {
         super(props);
+        if (unDef(props.options.endpoint)) {
+            console.error('GridSelector: No endpoint found');
+            return false;
+        }
+        if (isDef(props.options.sort) && typeof props.options.sort !== 'function') {
+            console.error('GridSelector: options.sort is not a function');
+            return false;
+        }
     }
     onClick({target}) {
         return this.props.options.resolve(target);
     }
     componentWillMount() {
-        // this.setState({width: `${document.querySelector(this.props.options.domElement).getBoundingClientRect().width}px`});
         updateStateFromFetch(this);
     }
     render() {
         const {onClick, props, state} = this;
         const className = `ts-grid-selector`;
         return (
-            <Grid fluid>
-                <Row className={className}>
-                    {factory(state.data)({onClick: props.options.resolve, ...props.options})(ContentBuilder)}
-                </Row>
-            </Grid>
+            <div className="container">
+                <Grid fluid>
+                    <Row className={className}>
+                        {factory(state.data)({onClick: props.options.resolve, ...props.options})(ContentBuilder)}
+                    </Row>
+                </Grid>
+            </div>
         );
     }
 };
