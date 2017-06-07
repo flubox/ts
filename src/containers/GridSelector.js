4efb8565f4e8328897fc2ad403205f1176722ced
flubox <francois.lunette@photobox.com>
2017-06-07 18:07:03 +0200

    better tracking options handling, code placeholder for srcSet

--- src/containers/GridSelector.js
+++ src/containers/GridSelector.js
@@ -1,11 +1,16 @@
 import React, {PropTypes, Component} from 'react';
 import autobind from 'autobind-decorator';
-import {factory, getLocale, getScreenResolution, getViewport, isDef, standardize, unDef} from '../helper';
+import {factory, getScreenResolution, getViewport, isDef, standardize, unDef} from '../helper';
 import ContentBuilder from '../ContentBuilder';
 import Loading from '../components/Loading';
 import {category, fetchInit, name} from '../constants';
 import { Grid, Row } from 'react-flexbox-grid';
 import ReactGA from 'react-ga';
+
+const configSchema = require('../../config.schema.json');
+const events = require('../../ga.events.json');
+const Ajv = require('ajv');
+const ajv = new Ajv();
 
 export const sort = sortFunction => data => typeof sortFunction === 'function' ? data.sort(sortFunction) : data;
 
@@ -24,67 +29,48 @@ export const updateStateFromFetch = context => {
 
 @autobind
 export class GridSelector extends Component {
-    state = {data: []}
+    state = {data: [], tracking: false}
     constructor(props) {
         super(props);
-        const {endpoint, locale, sort, tracking} = props.options;
-        this.state = {...this.state, name: props.options.name || name};
 
-        if (unDef(endpoint)) {
-            console.error('GridSelector: No endpoint found');
+        // Validate the config object provided via props.options
+        const validateConfig = ajv.compile(configSchema);
+        if (!validateConfig(props.options)) {
+            validateConfig.errors.forEach(({dataPath, keyword, message}) => {
+                return console.error(`GridSelector: options is invalid options${dataPath} ${message}`)
+            });
             return false;
         }
 
-        if (isDef(sort) && typeof sort !== 'function') {
-            console.error('GridSelector: options.sort is not a function');
-            return false;
-        }
+        if (isDef(props.options.tracking)) {
+            // Setting usable tracking 
+            this.state = {...this.state, events: {...events, ...props.options.tracking.events}};
 
-        if (isDef(tracking)) {
-            if (unDef(tracking.options)) {
-                console.error('GridSelector: options.tracking.options is required');
-                return false;
-            }
-            if (unDef(tracking.id)) {
-                console.error('GridSelector: options.tracking.id is undefined');
-                return false;
-            }
-            if (typeof tracking.id !== 'string') {
-                console.error('GridSelector: options.tracking.id is no a string');
-                return false;
-            }
-            if (tracking.id.match(/UA-([\d]{4,})-([\d]{1})/) === null) {
-                console.error('GridSelector: options.tracking.id doesn\'t match UA-XXXX-Y pattern');
-                return false;
-            }
-
+            // Mixing options for GA gtracking initialization
             const mixedOptions = {
-                ...tracking.options,
-                language: (tracking.options.language || locale || getLocale()).toLowerCase().replace('_', '-'),
+                ...props.options.tracking.options,
+                debug: props.options.debug,
                 screenResolution: getScreenResolution(),
                 viewportSize: getViewport()
             };
-            console.info('mixedOptions', mixedOptions);
-            ReactGA.initialize(tracking.id, tracking.options);
+            ReactGA.initialize(props.options.tracking.id, props.options.tracking.options);
+
+            // Ready to track
+            this.state = {...this.state, tracking: true};
         }
     }
     onClick(value) {
-        ReactGA.event({
-            category,
-            action: `Clicked on ${this.state.name} "${value}"`,
-            value,
-            hitCallback: () => this.props.options.resolve(value)
-        });
+        const {props, state} = this;
+        const {options} = props;
+        const {events, tracking} = state;
+        value = Number(value);
+        return tracking ? ReactGA.event({...events.onClick, value, hitCallback: () => options.resolve(value)}) : this.props.options.resolve(value);
     }
     componentWillMount() {
         updateStateFromFetch(this);
     }
     componentDidMount(nextProps) {
-        ReactGA.event({
-            category,
-            action: `View the "${this.state.name}" Selector Page`,
-            nonInteraction: true
-        });
+        return this.state.tracking && isDef(this.state.events.onLoad) && ReactGA.event({...this.state.events.onLoad, event: this.state.events.onLoad});
     }
     render() {
         const {onClick, props, state} = this;
