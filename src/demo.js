950e59919e7ab5a12f1a5dc873bb4e1ce3c2a6ab
flubox <francois.lunette@photobox.com>
2017-06-08 13:50:16 +0200

    dist

--- src/demo.js
+++ src/demo.js
@@ -41,28 +41,28 @@ const options = {
     // endpoint: 'http://localhost:8080/fakeapi',
     endpoint: () => {
         // Using local fakeapi after `npm run api`
-        return fetch('http://localhost:8080/fakeapi')
-        .then(data => data.json())
-        .then(data => {
-            if (debug) console.info('data', data);
-            return new Promise(resolve => {
-                setTimeout(() => {
-                    resolve({json: () => ({then: resolve => resolve(data)})});
-                }, 1000);
-            });
-        });
-        // Using github
-        // return fetch('https://api.github.com/repos/flubox/ts/contents/demo?ref=master')
+        // return fetch('http://localhost:8080/fakeapi')
         // .then(data => data.json())
         // .then(data => {
-        //     data = data.map((each, id) => ({id, url: [each.download_url]}));
+        //     if (debug) console.info('data', data);
         //     return new Promise(resolve => {
-        //         //Fake adjustable delay
         //         setTimeout(() => {
         //             resolve({json: () => ({then: resolve => resolve(data)})});
         //         }, 1000);
         //     });
         // });
+        // Using github
+        return fetch('https://api.github.com/repos/flubox/ts/contents/demo?ref=master')
+        .then(data => data.json())
+        .then(data => {
+            data = data.map((each, id) => ({id, url: [each.download_url]}));
+            return new Promise(resolve => {
+                //Fake adjustable delay
+                setTimeout(() => {
+                    resolve({json: () => ({then: resolve => resolve(data)})});
+                }, 1000);
+            });
+        });
     },
     locale: 'en_UK',
     resolve: id => console.info(`resolve id: ${id}`),
