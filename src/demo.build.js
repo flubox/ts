c0fde5b778c44ba27285a6102be8d7634130e0d5
François Lunette (Photobox) <Francois.lunette@photobox.com>
2017-06-16 14:26:22 +0200

    css update, github preview compliant fake endpoint, assets update

---
+++ src/demo.build.js
@@ -0,0 +1,19 @@
+var restify = require('restify');
+var fs = require('fs');
+var a = 'http://localhost:8080/fakeimg/';
+var imgList = fs.readdirSync(__dirname + '/../demo/')
+.filter(f => f.match(/\.png$/))
+.map(f => ({
+    filename: `../demo/${f}`,
+    radix: f.match(/^(.+)_[\d]+/)[1],
+    resolution: parseInt(f.match(/([\d]+)px.png$/)[1], 10)
+}))
+.sort((a, b) => a.radix < b.radix ? -1 : 1)
+.reduce((accumulator, current, index, all) => {
+    if (index % 2 === 0) {
+        return accumulator.concat([{id: index / 2, url: [current.filename, all[index + 1].filename]}]);
+    }
+    return accumulator;
+}, [])
+;
+fs.writeFileSync(__dirname + '/demo.json', JSON.stringify(imgList));
