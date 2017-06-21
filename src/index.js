3a0412c00a1c260ac92121fd4ba439aabe2ab5ed
François Lunette (Photobox) <Francois.lunette@photobox.com>
2017-06-21 11:37:24 +0200

    ts object exposed to window (#66)

--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ import {GridSelector} from './containers/GridSelector';
 const onLoaded = options => render(<GridSelector options={options}/>, document.querySelector(options.domElement));
 const ts = options => document.addEventListener('DOMContentLoaded', () => onLoaded(options));
 
+if (window) {
+    window.ts = ts;
+}
+
 export default ts;
